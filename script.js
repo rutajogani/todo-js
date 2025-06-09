8d38d8207ec68eb281175f7bc3783cb04f6341c9
Ruta Jogani <ruta007jogani@gmail.com>
2025-06-09 20:32:30 +0530

    add: make new lines in html and js

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-function add_todo() {
-    const todo_input = document.getElementById("todo-input");
-    const text = todo_input.value.trim();
-    console.log("text", text);
-    
-    if (text === "") return;
+function addTodo() {
+  const todoInput = document.getElementById("todo-input");
+  const text = todoInput.value.trim();
 
-  const checkbox = document.createElement("checkbox");
-  checkbox.textContent = text;
+  if (text === "") return;
 
-//   const deleteBtn = document.createElement("button");
-//   deleteBtn = "✔️";
-//   deleteBtn = () => {li.};
+  const li = document.createElement("li");
+  li.classList.add("flex", "items-center", "gap-2", "justify-center");
+
+  const label = document.createElement("label");
+  label.setAttribute("for", "todoInput");
+  label.innerHTML = text;
+  label.textContent = text;
+
+  const checkbox = document.createElement("input");
+  checkbox.setAttribute("id", "todoInput");
+  checkbox.setAttribute("type", "checkbox");
+
+  const node = document.createElement("label");
+  const todos = document.getElementById("todo-list");
+  todos.appendChild(checkbox);
+  todos.appendChild(node);
+
+  li.appendChild(checkbox);
+  li.appendChild(label);
+
+  document.getElementById("todo-list").appendChild(li);
+
+  todoInput.value = "";
 }
