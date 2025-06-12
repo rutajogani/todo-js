8568d81e36887e2324ef7c1bf32a34664700ede9
Ruta Jogani <ruta007jogani@gmail.com>
2025-06-12 14:53:00 +0530

    add: some lines in js code

--- script.js
+++ script.js
@@ -9,38 +9,45 @@ const arr = [
   },
 ];
 
-arr.map((value, index) => {
-  const li = document.createElement("li");
-  li.classList.add("flex", "items-center", "gap-2", "justify-center");
-
-  const label = document.createElement("label");
-  label.setAttribute("for", "todoInput");
-  label.innerHTML = value.title;
-  label.textContent = value.title;
-
-  const checkbox = document.createElement("input");
-  checkbox.setAttribute("id", "todoInput");
-  checkbox.setAttribute("type", "checkbox");
-  checkbox.addEventListener("change", function () {
-    if (this.checked) {
-      label.classList.add("line-through");
-    } else {
-      label.classList.remove("line-through");
-    }
-  });
-
-  const node = document.createElement("label");
+function refreshTodos() {
   const todos = document.getElementById("todo-list");
-  todos.appendChild(checkbox);
-  todos.appendChild(node);
+  todos.innerHTML = "";
+  
+  arr.map((value, index) => {
+    const li = document.createElement("li");
+    li.classList.add("flex", "items-center", "gap-2", "justify-center");
 
-  li.appendChild(checkbox);
-  li.appendChild(label);
+    const label = document.createElement("label");
+    label.setAttribute("for", "todoInput");
+    label.innerHTML = value.title;
+    label.textContent = value.title;
 
-  document.getElementById("todo-list").appendChild(li);
+    const checkbox = document.createElement("input");
+    checkbox.setAttribute("id", "todoInput");
+    checkbox.setAttribute("type", "checkbox");
+    checkbox.addEventListener("change", function () {
+      if (this.checked) {
+        label.classList.add("line-through");
+      } else {
+        label.classList.remove("line-through");
+      }
+    });
 
-  todoInput.value = "";
-});
+    const node = document.createElement("label");
+    todos.appendChild(checkbox);
+    todos.appendChild(node);
+
+    li.appendChild(checkbox);
+    li.appendChild(label);
+
+    document.getElementById("todo-list").appendChild(li);
+
+    todoInput.value = "";
+  });
+}
+
+// first load
+refreshTodos();
 
 function addTodo() {
   const todoInput = document.getElementById("todo-input");
@@ -49,6 +56,9 @@ function addTodo() {
   if (text === "") return;
 
   arr.push({
-      
+    title: text,
+    status: false,
   });
+
+  refreshTodos();
 }
